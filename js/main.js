512f5398fe37af65fa6199f73d30a43c94d13ba5
Maichol <msibiriu@gmail.com>
2020-11-10 16:29:13 +0100

    inizio creazione array vue

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// javascript
+
+const todo = new Vue({
+  el: '#todo',
+  data: {
+    indexImg: 0,
+    img: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSmrg98woXL2VjNUuDx8bEOYPujv0FsiBMp3g&usqp=CAU",
+      "https://www.equinoxio.org/UserFiles/image/2011/12/20111221-comic1.jpg",
+      "https://i0.wp.com/www.nerdpool.it/wp-content/uploads/2018/12/avengers-endgame-promo-art-captain-marvel-avenger-1151690.jpeg?resize=960%2C960&ssl=1",
+      "https://i.pinimg.com/originals/12/36/ea/1236ea333b9554cd8365756351f1d248.jpg",
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/abf4cac3-b714-4504-9957-0529ad89c60a/d2ounnu-664c8107-9a7d-4758-b324-95fa351156cc.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYWJmNGNhYzMtYjcxNC00NTA0LTk5NTctMDUyOWFkODljNjBhXC9kMm91bm51LTY2NGM4MTA3LTlhN2QtNDc1OC1iMzI0LTk1ZmEzNTExNTZjYy5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.7FjdQoeTnUAtnW77otRgE-27P1wrZsmCH_CmUI9wEBE",
+      "https://c4.wallpaperflare.com/wallpaper/835/7/584/marvel-comics-spider-man-the-avengers-avengers-infinity-war-wallpaper-preview.jpg",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+    ],
+  },
+  methods:{
+
+  }
+});
